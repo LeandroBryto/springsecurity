0ccfa0b81764bd04c1b0485946f2142fe8b01395
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-02-09 09:50:42 -0300

    repositórios
    adiciona repositórios JPA para User, Tweet e Role

--- src/main/java/com/leandrodev/springsecurity/repository/TweetRepository.java
+++ src/main/java/com/leandrodev/springsecurity/repository/TweetRepository.java
@@ -2,6 +2,8 @@ package com.leandrodev.springsecurity.repository;
 
 import com.leandrodev.springsecurity.entities.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TweetRepository extends JpaRepository<Tweet,Long> {
 }
