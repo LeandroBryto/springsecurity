39e912c8389cdb0027bab230a7a8ce843e298ebe
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-02-11 21:00:11 -0300

    Adiciona endpoint para criação de tweets

---
+++ src/main/java/com/leandrodev/springsecurity/controller/TweetController.java
@@ -0,0 +1,41 @@
+package com.leandrodev.springsecurity.controller;
+
+import com.leandrodev.springsecurity.controller.dto.CreateTweerDto;
+import com.leandrodev.springsecurity.entities.Tweet;
+import com.leandrodev.springsecurity.repository.TweetRepository;
+import com.leandrodev.springsecurity.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class TweetController {
+
+    private final TweetRepository tweetRepository;
+
+    private final UserRepository userRepository;
+
+    public TweetController(TweetRepository tweetRepository, UserRepository userRepository) {
+        this.tweetRepository = tweetRepository;
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/tweets")
+    public ResponseEntity<Void> createTweet(@RequestBody CreateTweerDto dto ,
+                                            JwtAuthenticationToken token){
+
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+        var tweet = new Tweet();
+        tweet.setUser(user.get());
+        tweet.setContent(dto.content());
+
+        tweetRepository.save(tweet);
+
+        return ResponseEntity.ok().build();
+
+    }
+}
